c4bd94ecca0ad105756a3d99f823f684e48d24ec
makarovda1984 <55186812+makarovda1984@users.noreply.github.com>
2019-12-09 17:42:42 +0300

    modified:   parser.py -теперь умеет мимо SSL работать

--- parser.py
+++ parser.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
+import ssl
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 #Адрес страницы
 HTML_ADDRESS = "http://auto.mail.ru/catalog/"
  
-html_doc = urlopen(HTML_ADDRESS).read()
+html_doc = urlopen(HTML_ADDRESS, context=ctx).read()
 
 soup = BeautifulSoup(html_doc)
  
